a8a4553671be8ebcf472359fb2f594f525a4acfd
akm4545 <akm4545@naver.com>
2024-01-22 15:34:07 +0900

    chapter3

--- chapter3/kafka-streams/src/main/java/com/example/kstreamGlobalktableJoin/KStreamJoinGlobalKTable.java
+++ chapter3/kafka-streams/src/main/java/com/example/kstreamGlobalktableJoin/KStreamJoinGlobalKTable.java
@@ -28,9 +28,21 @@ public class KStreamJoinGlobalKTable {
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         StreamsBuilder builder = new StreamsBuilder();
+//      GlobalKTable 생성
+//        GlobalKTable = table 형태의 값을 모든 테스크들이 처리
+//        ex 파티션 0 -> 테스크 0, 1 / 파티션 1 -> 테스트 0, 1
+//        모든 데이터를 저장하고 사용하기 때문에 스트림즈 애플리케이션의 로컬 스토리지의 사용량 증가
+//        네트워크, 브로커에 부하가 생김
+//        작은 용량의 데이터일 경우에만 사용하는것이 좋음
+//        다만 리파티셔닝을 거쳐 코파티셔닝이 되도록 할 필요가 없음
         GlobalKTable<String, String> addressGlobalTable = builder.globalTable(ADDRESS_GLOBAL_TABLE);
         KStream<String, String> orderStream = builder.stream(ORDER_STREAM);
 
+//        조인
+//        첫번째 파라미터 = 조인 대상
+//        두번째 파라미터 = 조인 조건 GlobalKTable은 레코드 매칭시 KStream의 메세지 키와 메세지 값을 사용 가능하다
+//        ex) KStream value == GlobalKTable key
+//        세번째 파라미터 = 데이터 가공
         orderStream.join(addressGlobalTable,
                     (orderKey, orderValue) -> orderKey,
                     (order, address) -> order + " send to " + address)
