d00365e641807c649459b3d69aa065fa348890ae
akm4545 <akm4545@naver.com>
2024-01-22 11:29:32 +0900

    chapter3

--- chapter3/kafka-admin/src/main/java/com/example/KafkaAdminClient.java
+++ chapter3/kafka-admin/src/main/java/com/example/KafkaAdminClient.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
+//어드민 api를 사용하여 카브카 브로커에 접속해 내부 옵션을 확인하거나 설정한다
 public class KafkaAdminClient {
     private final static Logger logger = LoggerFactory.getLogger(KafkaAdminClient.class);
 
@@ -21,9 +22,10 @@ public class KafkaAdminClient {
     public static void main(String[] args) throws Exception {
         Properties configs = new Properties();
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-
+//        어드민 api 생성
         AdminClient admin = AdminClient.create(configs);
 
+//        브로커 정보 조회
         logger.info("== Get broker information");
         for (Node node : admin.describeCluster().nodes().get()){
             logger.info("node: {}", node);
@@ -38,6 +40,7 @@ public class KafkaAdminClient {
             });
         }
 
+//        파티션 정보 조회
         logger.info("== Get default num.partitions");
         for(Node node : admin.describeCluster().nodes().get()){
             ConfigResource cr = new ConfigResource(ConfigResource.Type.BROKER, node.idString());
@@ -51,21 +54,25 @@ public class KafkaAdminClient {
             logger.info("{}", numPartitionConfig.value());
         }
 
+//        토픽 목록 정보 조회
         logger.info("== Topic list");
         for(TopicListing topicListing : admin.listTopics().listings().get()){
             logger.info("{}", topicListing.toString());
         }
 
+//        특정 토픽의 정보 조회
         logger.info("== test topic information");
         Map<String, TopicDescription> topicInformation = admin.describeTopics(Collections.singletonList("test")).all().get();
         logger.info("{}", topicInformation);
 
+//        컨슈머 그릅 정보 조회
         logger.info("== Consumer group list");
         ListConsumerGroupsResult listConsumerGroups = admin.listConsumerGroups();
         listConsumerGroups.all().get().forEach(v -> {
             logger.info("{}", v);
         });
 
+//        리소스 낭비를 방지하기 위해 명시적으로 종료
         admin.close();
     }
 }
