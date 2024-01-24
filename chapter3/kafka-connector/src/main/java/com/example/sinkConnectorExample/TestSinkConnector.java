6e93d4c7525279072781136974716a2dc0b6e785
akm4545 <akm4545@naver.com>
2024-01-24 11:50:40 +0900

    chapter3

--- chapter3/kafka-connector/src/main/java/com/example/sinkConnectorExample/TestSinkConnector.java
+++ chapter3/kafka-connector/src/main/java/com/example/sinkConnectorExample/TestSinkConnector.java
@@ -7,32 +7,41 @@ import org.apache.kafka.connect.sink.SinkConnector;
 import java.util.List;
 import java.util.Map;
 
+//사용자 정의 클래스 선언 = 커넥트 호출명
 public class TestSinkConnector extends SinkConnector {
+
+    //    설정값 초기화
+//    올바른 값이 아니라면 ConnectException을 발생시켜 종료해야 한다
     @Override
     public void start(Map<String, String> props) {
 
     }
 
+//    커넥터의 사용 테스크 클래스 지정
     @Override
     public Class<? extends Task> taskClass() {
         return null;
     }
 
+//    테스크 개수가 2개 이상일 경우 각기 다른 옵션을 설정할 때 사용
     @Override
     public List<Map<String, String>> taskConfigs(int maxTasks) {
         return null;
     }
 
+//    커넥터 종료 로직
     @Override
     public void stop() {
 
     }
 
+//    커넥터가 사용할 설정값 정보를 받는다
     @Override
     public ConfigDef config() {
         return null;
     }
 
+    //    버전
     @Override
     public String version() {
         return null;
--- chapter3/kafka-connector/src/main/java/com/example/sinkConnectorExample/TestSinkTask.java
+++ chapter3/kafka-connector/src/main/java/com/example/sinkConnectorExample/TestSinkTask.java
@@ -1,27 +1,43 @@
 package com.example.sinkConnectorExample;
 
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
 
 import java.util.Collection;
 import java.util.Map;
 
+
 public class TestSinkTask extends SinkTask {
+
+//    테스크 버전
     @Override
     public String version() {
         return null;
     }
 
+//    테스크 시작 로직
     @Override
     public void start(Map<String, String> props) {
 
     }
 
+//    데이터를 일정 주기로 싱크 애플리케이션 또는 싱크 파일에 저장할 때 사용하는 로직
+//    레코드를 저장하는 로직을 넣을 수 있으며 이 경우에는 flush메서드 로직을 구현하지 않아도 된다
     @Override
     public void put(Collection<SinkRecord> records) {
 
     }
 
+//   put 후처리 메서드 
+//    ex) 몽고디비인 경우 put에서 insert 후 flush에서 commit을 수행하여 트랜잭션 종료
+    @Override
+    public void flush(Map<TopicPartition, OffsetAndMetadata> currentOffsets) {
+        
+    }
+
+//    테스크 종료 로직
     @Override
     public void stop() {
 
