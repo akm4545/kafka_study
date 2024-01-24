ed4bca456c4e0de91dedf325e70b697bd0eabbc9
akm4545 <akm4545@naver.com>
2024-01-24 14:35:06 +0900

    chapter4

---
+++ chapter4/idempotence-producer/src/main/java/com/example/idempotenceproducer/IdempotenceProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.idempotenceproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotenceProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdempotenceProducerApplication.class, args);
+    }
+
+}
