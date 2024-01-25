927eef8b15fa183388dc9d8fe3ee7e4ade257d3c
akm4545 <akm4545@naver.com>
2024-01-25 12:37:36 +0900

    chapter4

---
+++ chapter4/kafka-multi-consumer-thread/src/main/java/com/example/kafkamulticonsumerthread/KafkaMultiConsumerThreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkamulticonsumerthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMultiConsumerThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMultiConsumerThreadApplication.class, args);
+    }
+
+}
---
+++ chapter4/kafka-multi-consumer-thread/src/test/java/com/example/kafkamulticonsumerthread/KafkaMultiConsumerThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkamulticonsumerthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMultiConsumerThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
