443ef22714e9fb7ab37fa22db5f4ebbd52f56b8f
akm4545 <akm4545@naver.com>
2024-01-25 11:19:19 +0900

    chapter4

--- chapter4/transaction-consumer/src/main/java/com/example/TransactionConsumer.java
+++ chapter4/transaction-consumer/src/main/java/com/example/TransactionConsumer.java
@@ -22,12 +22,20 @@ public class TransactionConsumer {
 
     private final static String GROUP_ID = "test-group";
 
+//    트랜잭션 프로듀서 설정
+//    다수의 파티션에 데이터를 저장할 경우 모든 데이터에 대해 동일한 원자성을 만족시키기 위해 사용된다
+//    동일 트랜잭션으로 묶어 전체 데이터를 처리하거나 처리하지 않도록 하는것을 의미
+//    기본 컨슈머 = 데이터가 파티션에 쌓이는 대로 모두 가져가 처리
+//    트랜잭션 컨슈머 = 트랜잭션으로 처리 완료된 데이터만 처리
+//    트랜잭션의 시작과 끝을 표현하기 위해 트랜잭션 레코드를 하나 더 보낸다
+//    트랜잭션 레코드는 파티션에 저장되어 오프셋을 한 개 차지한다
     public static void main(String[] args){
         Properties configs = new Properties();
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+//        트랜잭션 컨슈머 설정
         configs.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(configs);
