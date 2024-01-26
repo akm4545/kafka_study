26e04e340bf2f3ed26c5b01865ae2db9b012dfd0
akm4545 <akm4545@naver.com>
2024-01-26 17:23:07 +0900

    chapter4

--- chapter4/kafka-multi-consumer-thread/src/main/java/com/example/kafkamulticonsumerthread/ConsumerWorker.java
+++ chapter4/kafka-multi-consumer-thread/src/main/java/com/example/kafkamulticonsumerthread/ConsumerWorker.java
@@ -24,6 +24,7 @@ public class ConsumerWorker implements Runnable{
 
     private KafkaConsumer<String, String> consumer;
 
+//    KafkaConsuemr 인스턴스를 생성하기 위해 필요한 변수를 컨슈머 스레드 생성자 변수로 받는다
     ConsumerWorker(Properties prop, String topic, int number){
         this.prop = prop;
         this.topic = topic;
@@ -32,10 +33,14 @@ public class ConsumerWorker implements Runnable{
 
     @Override
     public void run() {
+//        KafkaConsumer 클래스는 스레드 세이프하지 않다.
+//        이 때문에 스레드별로 KafkaConsumer 인스턴스를 별개로 만들어서 운영해야만 한다
+//        만약 KafkaConsumer 인스턴스를 여러 스레드에서 실행하면 ConcurrentModificationExceoption 예외가 발생한다
         consumer = new KafkaConsumer<String, String>(prop);
         consumer.subscribe(Arrays.asList(topic));
 
         while (true){
+//            poll메서드를 통해 리턴받은 레코드들을 처리한다
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
 
             for(ConsumerRecord<String, String> record : records){
--- chapter4/kafka-multi-consumer-thread/src/main/java/com/example/kafkamulticonsumerthread/MultiConsumerThread.java
+++ chapter4/kafka-multi-consumer-thread/src/main/java/com/example/kafkamulticonsumerthread/MultiConsumerThread.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+//파티션 개수만큼 스레드 개수를 늘리는 멀티 스레드 전략
 public class MultiConsumerThread {
     private final static String TOPIC_NAME = "test";
 
@@ -14,6 +15,7 @@ public class MultiConsumerThread {
 
     private final static String GROUP_ID = "test-group";
 
+//    스레드 개수 변수
     private final static int CONSUMER_COUNT = 3;
 
     public static void main(String[] args){
@@ -25,6 +27,7 @@ public class MultiConsumerThread {
 
         ExecutorService executorService = Executors.newCachedThreadPool();
 
+//        3개의 컨슈머 스레드를 생성한다
         for(int i=0; i<CONSUMER_COUNT; i++){
             ConsumerWorker worker = new ConsumerWorker(configs, TOPIC_NAME, i);
 
