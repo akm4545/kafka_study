abe099ea69cd5a70e3f317b50d0eb1727bdae52c
akm4545 <akm4545@naver.com>
2024-01-29 11:48:40 +0900

    chapter4

--- chapter4/spring-kafka-producer/src/main/java/com/example/SpringProducerApplication.java
+++ chapter4/spring-kafka-producer/src/main/java/com/example/SpringProducerApplication.java
@@ -11,6 +11,8 @@ public class SpringProducerApplication implements CommandLineRunner {
 
     private static String TOPIC_NAME = "test";
 
+//    KafkaTemplate 주입 
+//    application.yaml의 설정값이 들어간다
     @Autowired
     private KafkaTemplate<Integer, String> template;
 
@@ -22,6 +24,7 @@ public class SpringProducerApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         for (int i=0; i<10; i++){
+//            sned 메서드로 토픽 이름과 메시지 값을 넣어 전송
             template.send(TOPIC_NAME, "test" + i);
         }
 
