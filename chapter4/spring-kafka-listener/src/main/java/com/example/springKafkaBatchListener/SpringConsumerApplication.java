9c00561475461dc71c6a487de67438d12a121786
akm4545 <akm4545@naver.com>
2024-01-29 16:03:20 +0900

    chapter4

--- chapter4/spring-kafka-listener/src/main/java/com/example/springKafkaBatchListener/SpringConsumerApplication.java
+++ chapter4/spring-kafka-listener/src/main/java/com/example/springKafkaBatchListener/SpringConsumerApplication.java
@@ -10,6 +10,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 
 import java.util.List;
 
+//배치 리스너
+//poll 메서드로 호출된 레코드가 모두 처리된 다음 커밋
+//KafkaListener 와 다르게 메서드의 파라미터를 List 또는 ConsumerRecords로 받는다 (배치이기 때문에)
 @SpringBootApplication
 public class SpringConsumerApplication {
     public static Logger logger = LoggerFactory.getLogger(SpringConsumerApplication.class);
@@ -19,16 +22,20 @@ public class SpringConsumerApplication {
         application.run(args);
     }
 
+//    컨슈머 레코드의 묶음을 파라미터로 받는다 
+//    카프카 클리이언트 라이브러리에서 poll 메서드로 리턴받은 ConsumerRecords를 리턴받아 사용하는것과 동일
     @KafkaListener(topics = "test", groupId = "test-group-01")
     public void batchListener(ConsumerRecords<String, String> records){
         records.forEach(record -> logger.info(record.toString()));
     }
 
+//    메세지 값들을 List 자료구조로 받아 처리
     @KafkaListener(topics = "test", groupId = "test-group-02")
     public void batchListener(List<String> list){
         list.forEach(recordValue -> logger.info(recordValue));
     }
 
+//    2개 이상의 컨슈머 스레드로 배치 리스너를 운영할 경우 concurrency 옵션 추가
     @KafkaListener(topics = "test", groupId = "test-group-03", concurrency = "3")
     public void concurrentBatchListener(ConsumerRecords<String, String> records){
         records.forEach(record -> logger.info(record.topic()));
