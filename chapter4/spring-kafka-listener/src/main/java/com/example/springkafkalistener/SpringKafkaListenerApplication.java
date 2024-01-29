0cec879f7b589c803361f3c895a67db8b7dabbad
akm4545 <akm4545@naver.com>
2024-01-29 12:12:35 +0900

    chapter4

---
+++ chapter4/spring-kafka-listener/src/main/java/com/example/springkafkalistener/SpringKafkaListenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkalistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaListenerApplication.class, args);
+    }
+
+}
---
+++ chapter4/spring-kafka-listener/src/test/java/com/example/springkafkalistener/SpringKafkaListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkalistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
