aeaae409707149665cd9c8bc593558e0d79fdb44
akm4545 <akm4545@naver.com>
2024-01-31 13:23:12 +0900

    chapter5

--- chapter5/web-page-event-pipeline/src/main/java/com/pipline/RestApiProducer.java
+++ chapter5/web-page-event-pipeline/src/main/java/com/pipline/RestApiProducer.java
@@ -1,4 +1,16 @@
 package com.pipline;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
 public class RestApiProducer {
+    public static void main(String[] args){
+        SpringApplication.run(RestApiProducer.class, args);
+    }
 }
--- chapter5/web-page-event-pipeline/src/main/java/com/pipline/UserEventVO.java
+++ chapter5/web-page-event-pipeline/src/main/java/com/pipline/UserEventVO.java
@@ -1,4 +1,25 @@
 package com.pipline;
 
 public class UserEventVO {
+
+    public UserEventVO(String timestamp, String userAgent, String colorName, String userName){
+        this.timestamp = timestamp;
+        this.userAgent = userAgent;
+        this.colorName = colorName;
+        this.userName = userName;
+    }
+
+//    데이터 생성 시점을 저장 
+//    적재 순서가 순차적이지 않더라고 해당 값으로 정렬 가능
+    private String timestamp;
+
+//    유저 사용 브라우저
+    private String userAgent;
+
+//    컬러명
+    private String colorName;
+
+//    유저명
+    private String userName;
+
 }
