9fdeaa3bd1c6569684ace314b8e3e6d5e34e9c21
akm4545 <akm4545@naver.com>
2024-02-05 17:00:37 +0900

    chapter5

---
+++ chapter5/elasticsearch-kafka-connector/src/main/java/com/example/elasticsearchkafkaconnector/ElasticsearchKafkaConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchkafkaconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchKafkaConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchKafkaConnectorApplication.class, args);
+    }
+
+}
---
+++ chapter5/elasticsearch-kafka-connector/src/test/java/com/example/elasticsearchkafkaconnector/ElasticsearchKafkaConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchkafkaconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchKafkaConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
