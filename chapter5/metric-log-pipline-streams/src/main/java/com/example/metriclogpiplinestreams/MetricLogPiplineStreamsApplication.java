22018faf176f20654085fd9687388276bccfa405
akm4545 <akm4545@naver.com>
2024-02-05 18:41:57 +0900

    chapter5

---
+++ chapter5/metric-log-pipline-streams/src/main/java/com/example/metriclogpiplinestreams/MetricLogPiplineStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.metriclogpiplinestreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricLogPiplineStreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricLogPiplineStreamsApplication.class, args);
+    }
+
+}
---
+++ chapter5/metric-log-pipline-streams/src/test/java/com/example/metriclogpiplinestreams/MetricLogPiplineStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metriclogpiplinestreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricLogPiplineStreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
