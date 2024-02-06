a5b10a858a11a5888135cbf493de68cf1c86ece6
akm4545 <akm4545@naver.com>
2024-02-06 12:10:37 +0900

    chapter6

---
+++ chapter5/confluent-kafka/src/main/java/com/example/confluentkafka/ConfluentKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.confluentkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfluentKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfluentKafkaApplication.class, args);
+    }
+
+}
